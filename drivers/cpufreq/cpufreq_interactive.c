443eb28213e170ac8e092f16efba3d03ac3fae60
motley <motley.slate@gmail.com>
2012-11-26 22:01:57 -0500

    cpufreq: governor tweaks

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -71,7 +71,7 @@ static struct mutex set_speed_lock;
 static u64 hispeed_freq;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 90
+#define DEFAULT_GO_HISPEED_LOAD 80
 static unsigned long go_hispeed_load;
 
 /*
